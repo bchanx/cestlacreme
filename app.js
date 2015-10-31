30976fb961ac3ddfb007094fa76d4dc57bd01045
bchanx <bchanx@gmail.com>
2015-10-31 03:39:55 -0700

    Setup gulp and other things

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.set('port', (process.env.PORT || 3000));
-app.set('env', (process.env.ENV || 'development'));
+app.set('env', (process.env.NODE_ENV || 'development'));
 
 app.use(favicon(__dirname + '/static/favicon.ico'));
 app.use(bodyParser.json());
@@ -17,10 +17,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'static')));
 
+app.locals = {
+  env: app.get('env'),
+  min: app.get('env') === 'production' ? '.min' : ''
+};
+
 app.get('/', function (req, res) {
   res.render('index');
 });
 
 var server = app.listen(app.get('port'), function () {
-  console.log('Server running on port:', app.get('port'));
+  console.log('Server running as [%s] on port [%s]', app.get('env'), app.get('port'));
 });
