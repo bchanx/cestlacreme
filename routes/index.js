803121a7ab453380e73be45999d09fc8e3adcb60
bchanx <bchanx@gmail.com>
2015-12-07 02:45:23 -0800

    remove splash page

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 var router = require('express').Router();
-var pg = require('pg');
-var orders = require('../models/orders');
 
+/*
 if (process.env.NODE_ENV === 'production') {
   router.get('/', function(req, res) {
     res.render('splash');
   });
 }
+*/
 
 router.get('/splash', function(req, res) {
   res.render('splash');
