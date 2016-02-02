b162cf6f0f3ce576f850615360ba9e969649e14e
bchanx <bchanx@gmail.com>
2016-02-02 10:29:29 -0800

    update

--- app/components/Home.js
+++ app/components/Home.js
@@ -35,7 +35,7 @@ var Home = React.createClass({
           <Bold>Next scheduled pickup date:</Bold>
           <br/>
           <div className="pickup-date">
-            <Bold>Thursday, January 28th, 6pm - 7pm</Bold>
+            <Bold>Thursday, February 11th, 6pm - 7pm</Bold>
           </div>
         </div>
         <Break/>
